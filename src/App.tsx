1de856291b760b76eac0a02ce7cce40be600dfc6
Robert <celadoragroup@gmail.com>
2022-05-06 07:53:57 -0700

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import PeriodicTable from '@celadora/periodic-table';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span className="name">Robert Rooney</span>
+      </header>
+
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div style={{ textAlign: 'center' }}>
+          <p>Periodic Table of Elements</p>
+          <PeriodicTable />
+          <div>
+            <p>Github: <a href="https://github.com/LagSlug/PeriodicTable-React">https://github.com/LagSlug/PeriodicTable-React</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
